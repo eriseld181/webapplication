69b2f94eaa96438b638ad0697ab7abf80974c966
eriseld181 <49815928+eriseld181@users.noreply.github.com>
2021-04-21 11:23:48 +0200

    update

--- src/main/java/com/crystal/webapplication/mappers/CommentMapper.java
+++ src/main/java/com/crystal/webapplication/mappers/CommentMapper.java
@@ -12,12 +12,12 @@ public class CommentMapper {
 //mer nje object nga db
 //mapoje sipas objektit dto (konverto)-funksion qe mer nje comment model edhe kthen dto: krijohet objekti i ri
 //do ta therrasim tek controller
-	
+
 //	CommentMapper commentMapper= Mappers.getMapper(CommentMapper.class);
 //	
 //	@Mapping(source="comment_text", target="comment_text_dto")
 //	CommentDto mappCommentToDto(Comment comment);
-	
+
 //	public CommentDto mappCommentToDto(Comment comment) {
 //		CommentDto commentDto= new CommentDto(comment.getComment_text(), comment.getComment_author(), comment.getComment_date());
 //		return commentDto;
