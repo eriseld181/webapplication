6c2098ec11b61d3d687a149c6067da70b0fbfa60
eriseld181 <49815928+eriseld181@users.noreply.github.com>
2021-04-21 15:51:57 +0200

    update news services

--- src/main/java/com/crystal/webapplication/services/NewsServices.java
+++ src/main/java/com/crystal/webapplication/services/NewsServices.java
@@ -2,32 +2,58 @@ package com.crystal.webapplication.services;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.crystal.webapplication.controllers.NewsController;
+
 import com.crystal.webapplication.dto.NewsDto;
 import com.crystal.webapplication.mappers.NewsMapper;
 import com.crystal.webapplication.models.News;
 import com.crystal.webapplication.repositories.NewsRepository;
 
-
 //tregon qe eshte per services
 @Service
+
 public class NewsServices {
-	//krijon objektin
+
+	// krijon objektin
 	@Autowired
 	private NewsRepository newsRepository;
-	
-	public List<News> list(){
+	//private NewsMapper newsMapper;
+
+	public List<News> list() {
 		return newsRepository.findAll();
 	}
-	//@GetMapping
-	//NewsController.deleteOne();
+
+	public void deleteOne(Long id) {
+
+		newsRepository.deleteById(id);
+	}
+
+	// funsjoni per per te krijuar nje news te ri
+	public News create( News news) {
+		return newsRepository.saveAndFlush(news);
+	}
+	//funksjoni per te bere update nje specific news
+	public News updateOne( Long id,  News news) {
+	 	News exisitingNews = newsRepository.getOne(id);
+		BeanUtils.copyProperties(news, exisitingNews, "id");
+	 	return newsRepository.saveAndFlush(exisitingNews);
+		
+	}
+	public News getFirst( Long id) {
+		return newsRepository.getOne(id);
+	}
 	
-	
+	public NewsDto getSome(Long id) {
+		NewsMapper newsM = new NewsMapper();
+		return newsM.modelMapper(newsRepository.getOne(id));
+	}
+
 }
