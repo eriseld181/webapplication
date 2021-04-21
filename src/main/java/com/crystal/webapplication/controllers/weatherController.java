b618ea6a00521ddde101b0cd5306fd55112c29ae
Juli10s <82818895+Juli10s@users.noreply.github.com>
2021-04-21 17:39:49 +0200

    :(

--- src/main/java/com/crystal/webapplication/controllers/weatherController.java
+++ src/main/java/com/crystal/webapplication/controllers/weatherController.java
@@ -6,13 +6,16 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/weathers")
 public class weatherController {
     @Autowired
-    private weatherRepository weatherRepository;
+    private WeatherRepository weatherRepository;
 
     @GetMapping
     public List<Weather> list(){
@@ -20,20 +23,23 @@ public class weatherController {
     }
     @GetMapping
     @RequestMapping("{idweather}")
-    public Weather get(@PathVariable String idweather){
-        return weatherRepository.getOne(idweather);
+    public Weather get(@PathVariable String idweather) throws ParseException {
+        Date date = new SimpleDateFormat("dd/MM/yyyy").parse(idweather);
+        return weatherRepository.getOne(date);
     }
     @PostMapping
     public Weather create(@RequestBody final Weather weather){
         return weatherRepository.saveAndFlush(weather);
     }
-    @RequestMapping(value = "{id}",method = RequestMethod.DELETE)
-    public void delete(@PathVariable String id){
-        weatherRepository.deleteById(id);
+    @RequestMapping(value = "{idweather}",method = RequestMethod.DELETE)
+    public void delete(@PathVariable String idweather) throws ParseException {
+        Date date = new SimpleDateFormat("dd/MM/yyyy").parse(idweather);
+        weatherRepository.deleteById(date);
     }
-    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
-    public Weather update(@PathVariable String id, @RequestBody Weather weather){
-        Weather existingWeather = weatherRepository.getOne(id);
+    @RequestMapping(value = "{idweather}", method = RequestMethod.PUT)
+    public Weather update(@PathVariable String idweather, @RequestBody Weather weather) throws ParseException {
+        Date date = new SimpleDateFormat("dd/MM/yyyy").parse(idweather);
+        Weather existingWeather = weatherRepository.getOne(date);
         BeanUtils.copyProperties(weather,existingWeather,"idweather");
         return weatherRepository.saveAndFlush(existingWeather);
     }
