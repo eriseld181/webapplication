8ba68669ee8f59a743099a3665d590ac75c46d07
eriseld181 <49815928+eriseld181@users.noreply.github.com>
2021-04-22 09:56:10 +0200

    update news repository

--- src/main/java/com/crystal/webapplication/repositories/NewsRepository.java
+++ src/main/java/com/crystal/webapplication/repositories/NewsRepository.java
@@ -1,9 +1,15 @@
 package com.crystal.webapplication.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.crystal.webapplication.models.News;
 
-public interface NewsRepository extends JpaRepository<News ,Long>{
+public interface NewsRepository extends JpaRepository<News, Long> {
+	public static List<News> searchNews(String search) {
 
+		return null;
+
+	}
 }
