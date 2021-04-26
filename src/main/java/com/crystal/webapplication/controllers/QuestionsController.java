f7715506c2ac957f73bcc4f7692c000eae46bc43
elsson98 <elsonlleshi2@gmail.com>
2021-04-26 14:02:45 +0200

    final

--- src/main/java/com/crystal/webapplication/controllers/QuestionsController.java
+++ src/main/java/com/crystal/webapplication/controllers/QuestionsController.java
@@ -1,9 +1,10 @@
 package com.crystal.webapplication.controllers;
-
-import com.crystal.webapplication.dto.QuestionsDTO;
 import com.crystal.webapplication.models.Questions;
 import com.crystal.webapplication.services.QuestionsService;
+import com.crystal.webapplication.services.TopicsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
@@ -12,45 +13,86 @@ import java.util.List;
 public class QuestionsController {
     @Autowired
     private QuestionsService questionsService;
-private QuestionsDTO questionsDTO;
+
+
     @GetMapping
-    public List<Questions> list(){
-      return questionsService.list();
+    public ResponseEntity<List<Questions>> list() {
+        if(questionsService.list().isEmpty())
+            return  new ResponseEntity("There is no question in database.",HttpStatus.BAD_REQUEST);
+        return ResponseEntity.status(HttpStatus.OK).body(questionsService.list());
     }
 
-//    @GetMapping("{id}")
-//    public  Questions get(@PathVariable Long id){
-//       return questionsService.getOneRecord(id);
-//    }
+        @GetMapping("{id}")
+    public  ResponseEntity<Questions> get(@PathVariable Long id){
+    if(questionsService.existById(id)) {
+        return ResponseEntity.status(HttpStatus.OK).body(questionsService.getOneRecord(id));
+    }else
+            return  new ResponseEntity("Question not found.",HttpStatus.BAD_REQUEST);
+        }
+
     @PostMapping
-    public Questions create(@RequestBody final Questions questions){
-        return questionsService.createQuestions(questions);
-    }
-    @DeleteMapping(value = "{id}" )
-    public void delete(@PathVariable Long id) {
-        questionsService.deleteQuestions(id);
+    public ResponseEntity create(@RequestBody final Questions questions) {
+        try {
+            questionsService.createQuestions(questions);
+            return new ResponseEntity("Question added succesfully.", HttpStatus.OK);
+        }
+        catch (Exception e){
+            return new ResponseEntity("Error: question cant be added.", HttpStatus.BAD_REQUEST);
+        }
     }
 
-    @PutMapping(value = "/update/{id}" )
-    public Questions update(@PathVariable Long id, @RequestBody Questions questions) {
+    @DeleteMapping(value = "{id}")
+    public ResponseEntity delete(@PathVariable Long id) {
+        try {
+            questionsService.deleteQuestions(id);
+            return new ResponseEntity("Question deleted succesfully.", HttpStatus.OK);
+        } catch (Exception ex) {
+            return new ResponseEntity("Error: question dofesn't exist.", HttpStatus.BAD_REQUEST);
+        }
+    }
 
-        return questionsService.update(id,questions);
+    @PutMapping(value = "/update/{id}")
+    public ResponseEntity update(@PathVariable Long id, @RequestBody Questions questions) {
+        try {
+            questionsService.update(id, questions);
+            return new ResponseEntity("Question is updated.", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity("Error:   question can not be updated.", HttpStatus.SEE_OTHER );
+        }
     }
-    @PutMapping(value = "/updateResponse/{id}" )
-    public Questions updateResponse(@PathVariable (value="id") Long id,@RequestBody Questions questions){
-        return  questionsService.updateResponse(id,questions);
+
+    @PutMapping(value = "/updateResponse/{id}")
+    public ResponseEntity updateResponse(@PathVariable(value = "id") Long id, @RequestBody Questions questions) {
+      try {
+          questionsService.updateResponse(id, questions);
+          return new ResponseEntity("Question  response is updated.", HttpStatus.OK);
+      }
+      catch (Exception ex){
+          return new ResponseEntity("Error: response can not be updated.", HttpStatus.BAD_REQUEST);
+      }
     }
+
     @DeleteMapping("/deleteNotAnswered/{id}")
-    public void deleteNotAnswered(@PathVariable (value="id")  Long id) {
+    public ResponseEntity deleteNotAnswered(@PathVariable(value = "id") Long id) {
+        try{
         questionsService.deleteNotAnswered(id);
-    }
-    @GetMapping("/getNotAnswered")
-    public List<String> getNotAnswered(){
-        return questionsService.getNotAnswered();
-    }
-    @GetMapping("/findById/{id}")
-    public List<Questions> listNotAnsweredById( @PathVariable Long id){
-        return questionsService.findNotAnsweredByTopicId(id);
+            return new ResponseEntity("Questions without response deleted succesfully.", HttpStatus.OK);
+        } catch (Exception ex) {
+            return new ResponseEntity("All questions has response ,no question deleted.", HttpStatus.BAD_REQUEST);
+        }
     }
 
+    @GetMapping("/getNotAnswered")
+    public ResponseEntity<List<String>> getNotAnswered() {
+        if (questionsService.getNotAnswered().isEmpty())
+            return new ResponseEntity("There is no question without answer.", HttpStatus.BAD_REQUEST);
+        return ResponseEntity.status(HttpStatus.OK).body(questionsService.getNotAnswered());
+    }
+
+    @GetMapping("/findById/{id}")
+    public ResponseEntity<List<Questions>> listNotAnsweredById(@PathVariable Long id) {
+        if (questionsService.findNotAnsweredByTopicId(id).isEmpty())
+            return new ResponseEntity("There is no question without answer for this topic.", HttpStatus.BAD_REQUEST);
+        return ResponseEntity.status(HttpStatus.OK).body(questionsService.findNotAnsweredByTopicId(id));
+    }
 }
--- src/main/java/com/crystal/webapplication/repositories/QuestionsRepository.java
+++ src/main/java/com/crystal/webapplication/repositories/QuestionsRepository.java
@@ -6,15 +6,21 @@ import org.springframework.data.jpa.repository.Query;
 
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
 
 public interface QuestionsRepository  extends JpaRepository <Questions,  Long>{
+
    @Query( value="select u.question_name  from questions  u where u.answer is NULL")
    List<String> showNotAnswered();
 
+   @Query( value="select u.id  from questions  u ")
+   ArrayList<Integer> getAllId(Long id);
+
  @Query( value="select u from questions as u where u.topic_id =?1 and u.answer is null")
     List <Questions> findNotAnsweredByTopicId(Long topic_id);
-    @Transactional
+
+   @Transactional
     @Modifying
     @Query( value="delete from questions as u where u.id=?1 and u.answer is NULL ")
     void  deleteNotAnswered(Long id);
